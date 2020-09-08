1c2a7b34c60a1676b100dd44439ac5156d07a60a
suraj kumar <studmuffinat18@gmail.com>
2020-09-08 14:07:26 +0530

    chnaged port

--- app.js
+++ app.js
@@ -135,6 +135,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server running");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("The server has started successfully");
+})
